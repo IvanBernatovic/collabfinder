119616efbc06860f050eeb97f029166d5b0af7bc
Ivan Bernatović <ivan.bernatovic.93@gmail.com>
2020-12-31 11:08:45 +0100

    Fix copy in email

--- resources/views/emails/projects/user-applied.blade.php
+++ resources/views/emails/projects/user-applied.blade.php
@@ -1,12 +1,12 @@
 @component('mail::message')
-# Hi, {{ $project->user->name }}!
+    # Hi, {{ $project->user->name }}!
 
-User {{ $user->name }} applied to your project "{{ $project->name }}".
+    User {{ $user->name }} applied for your project "{{ $project->name }}".
 
-@component('mail::panel')
-{{ $user->applicationForProject($project)->pivot->message }}    
-@endcomponent
+    @component('mail::panel')
+        {{ $user->applicationForProject($project)->pivot->message }}
+    @endcomponent
 
-Thanks,<br>
-{{ config('app.name') }}
+    Thanks,<br>
+    {{ config('app.name') }}
 @endcomponent
