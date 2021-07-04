1c6fdb0d87f4c2ace7e5d54bbe690c0164b1efc5
Ivan Bernatović <ivan.bernatovic.93@gmail.com>
2021-07-04 18:56:01 +0200

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,30 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    purge: [
-        "./vendor/laravel/jetstream/**/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./resources/js/**/*.vue"
-    ],
+  purge: [
+    './vendor/laravel/jetstream/**/*.blade.php',
+    './storage/framework/views/*.php',
+    './resources/views/**/*.blade.php',
+    './resources/js/**/*.vue'
+  ],
 
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Nunito", ...defaultTheme.fontFamily.sans]
-            }
-        }
-    },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Nunito', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
 
-    variants: {
-        opacity: ["responsive", "hover", "focus", "disabled"],
-        backgroundColor: ["responsive", "hover", "focus", "disabled"],
-        cursor: ["responsive", "disabled"]
-    },
+  variants: {
+    opacity: ['responsive', 'hover', 'focus', 'disabled'],
+    backgroundColor: ['responsive', 'hover', 'focus', 'disabled'],
+    cursor: ['responsive', 'disabled']
+  },
 
-    plugins: [require("@tailwindcss/ui")]
-};
+  plugins: [require('@tailwindcss/ui')],
+
+  future: {
+    purgeLayersByDefault: true
+  }
+}
