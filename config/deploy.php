82d512867eec35eefd2ff9dc407fd8dfd33177d5
Ivan Bernatović <ivan.bernatovic.93@gmail.com>
2025-02-12 09:57:36 +0100

    Update deployer PHP version

--- config/deploy.php
+++ config/deploy.php
@@ -98,8 +98,8 @@ return [
     'options' => [
         'application' => env('APP_NAME', 'Laravel'),
         'repository' => 'git@github.com:IvanBernatovic/collabfinder.git',
-        'bin/php' => '/usr/bin/php7.4',
-        'bin/composer' => '/usr/bin/php7.4 /usr/local/bin/composer',
+        'bin/php' => '/usr/bin/php8.1',
+        'bin/composer' => '/usr/bin/php8.1 /usr/local/bin/composer',
         'composer_options' => '{{composer_action}} --verbose --prefer-dist --no-progress --no-interaction --no-dev --optimize-autoloader',
         'env' => [
             'PATH' => env('DEPLOYER_ENV_PATH', '')
