95962524bbd2a1a181134a57a2008938220c245f
Ivan Bernatović <ivan.bernatovic.93@gmail.com>
2025-09-07 16:29:07 +0000

    Add comments migrations

---
+++ database/migrations/2025_09_07_112013_add_parent_id_to_comments_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->softDeletes();
+            $table->foreignId('parent_id')->nullable()->constrained('comments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropSoftDeletes();
+            $table->dropConstrainedForeignId('parent_id');
+        });
+    }
+};
