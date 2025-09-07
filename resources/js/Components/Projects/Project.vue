6fc12c43463eaafbbac003a449a35ec298c63efb
Ivan Bernatović <ivan.bernatovic.93@gmail.com>
2025-09-07 07:55:00 +0000

    Fix CSS issue when there's a lot of roles listed

--- resources/js/Components/Projects/Project.vue
+++ resources/js/Components/Projects/Project.vue
@@ -2,44 +2,27 @@
   <div class="bg-white p-6 border-b border-gray-200">
     <div class="flex justify-between items-start">
       <div class="mr-4">
-        <InertiaLink
-          class="font-semibold text-[15px] text-black hover:underline"
-          :href="`/projects/${project.id}`"
-          >{{ project.name }}
+        <InertiaLink class="font-semibold text-[15px] text-black hover:underline" :href="`/projects/${project.id}`">{{
+          project.name }}
         </InertiaLink>
-        <p class="flex items-center mt-1">
-          <svg
-            v-if="project.roles.length === 1"
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5"
-            viewBox="0 0 20 20"
-            fill="#51CF66"
-          >
-            <path
-              fill-rule="evenodd"
-              d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
-              clip-rule="evenodd"
-            />
-          </svg>
+        <div class="flex mt-1">
+          <div>
+            <svg v-if="project.roles.length === 1" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5"
+              viewBox="0 0 20 20" fill="#51CF66">
+              <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+            </svg>
 
-          <svg
-            v-if="project.roles.length > 1"
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-10 w-10"
-            viewBox="0 0 20 20"
-            fill="#51CF66"
-            height="22px"
-            width="22px"
-          >
-            <path
-              d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z"
-            />
-          </svg>
+            <svg v-if="project.roles.length > 1" xmlns="http://www.w3.org/2000/svg" className="h-10 w-10"
+              viewBox="0 0 20 20" fill="#51CF66" height="22px" width="22px">
+              <path
+                d="M9 6a3 3 0 11-6 0 3 3 0 016 0zM17 6a3 3 0 11-6 0 3 3 0 016 0zM12.93 17c.046-.327.07-.66.07-1a6.97 6.97 0 00-1.5-4.33A5 5 0 0119 16v1h-6.07zM6 11a5 5 0 015 5v1H1v-1a5 5 0 015-5z" />
+            </svg>
+          </div>
 
-          <span class="ml-2">{{
+          <p class="ml-2">{{
             project.roles.map(role => role.title).join(', ')
-          }}</span>
-        </p>
+          }}</p>
+        </div>
       </div>
 
       <div class="actions">
@@ -48,18 +31,11 @@
           <BookmarkIconSolid class="w-4 h-4" v-if="saved" />
         </button>
 
-        <InertiaLink
-          :href="`/projects/${project.id}/edit`"
-          :data="{ backLink }"
-          v-if="project.user.id === user.id"
-        >
+        <InertiaLink :href="`/projects/${project.id}/edit`" :data="{ backLink }" v-if="project.user.id === user.id">
           <PencilIcon class="w-4 h-4" />
         </InertiaLink>
 
-        <button
-          v-if="project.user.id === user.id"
-          @click="$emit('deleteProject', project)"
-        >
+        <button v-if="project.user.id === user.id" @click="$emit('deleteProject', project)">
           <TrashIcon class="w-4 h-4" />
         </button>
       </div>
@@ -67,11 +43,8 @@
 
     <div class="flex items-center justify-between mt-6">
       <div class="flex flex-wrap gap-2 mr-3">
-        <div
-          class="bg-[#f1f1f1] text-[#3d3d3d] font-light px-2 py-0.5 rounded"
-          v-for="tag in project.tags"
-          :key="tag.id"
-        >
+        <div class="bg-[#f1f1f1] text-[#3d3d3d] font-light px-2 py-0.5 rounded" v-for="tag in project.tags"
+          :key="tag.id">
           {{ tag.name }}
         </div>
       </div>
@@ -111,7 +84,7 @@ const createdAtDiff = dayjs(props.project.created_at).fromNow()
 .actions {
   @apply flex gap-1.5;
 
-  > * {
+  >* {
     @apply text-gray-500 border border-gray-200 rounded p-1.5;
   }
 }
